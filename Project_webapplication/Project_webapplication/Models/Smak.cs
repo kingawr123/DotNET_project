97b09d795d88f2f9b3bd066891e113b75b709bff
Kinga Wrona <kingawr123@gmail.com>
2023-06-01 11:13:12 +0200

    new start

--- Project_webapplication/Project_webapplication/Models/Smak.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace Project_webapplication.Models;
-
-public class Smak
-{
-    [Key] public int IdSmaku { get; set; }
-    [Display(Name = "Nazwa")] public string Nazwa { get; set; }
-}
