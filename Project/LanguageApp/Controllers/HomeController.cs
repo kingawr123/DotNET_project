5ab0a23bed208281c19c7f4dd89b5714362e410f
katstep <otiofthebag@protonmail.com>
2023-06-01 12:18:04 +0200

    początek loginu

--- Project/LanguageApp/Controllers/HomeController.cs
+++ Project/LanguageApp/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace LanguageApp.Controllers;
 
 public class HomeController : Controller
 {
+
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
@@ -18,14 +19,10 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Login(LoginVM model)
     {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        string Username = model.Username;
+        string Password = model.Password;
+        return View("Login", model);
     }
 }
---
+++ Project/LanguageApp/Models/LoginVM.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageApp.Models
+{
+    public class LoginVM
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
